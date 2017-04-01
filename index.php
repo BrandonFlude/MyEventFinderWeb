290b2813a3165cb49af97627548978eaa9e21d93
Brandon Flude <BrandonFlude@users.noreply.github.com>
2017-04-02 00:05:36 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,81 @@
+<?php include($_SERVER['DOCUMENT_ROOT'].'/assets/containers/head.php'); ?>
+  <title>My Event Finder</title>
+</head>
+<body id="page-top">
+  <?php include($_SERVER['DOCUMENT_ROOT'].'/assets/containers/nav.php'); ?>
+  <header>
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-7">
+          <div class="header-content">
+            <div class="header-content-inner">
+              <h1>Never miss out on your teams' sport events. Simply select the teams you follow and we'll do the rest.</h1>
+              <a href="/signup" class="btn btn-outline btn-xl page-scroll">Start Now for Free!</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-5">
+          <div class="device-container">
+            <img src="/assets/img/android-phone.png" class="img-responsive" alt="Android Phone">
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+
+  <section id="features" class="features">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12 text-center">
+          <div class="section-heading">
+            <h2>Your All-In-One Calendar</h2>
+            <p class="text-muted">See all the events you care about in one place.</p>
+            <hr>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-12">
+          <div class="container-fluid">
+            <div class="row">
+              <div class="col-md-6">
+                <div class="feature-item">
+                  <span class="fa fa-fw fa-futbol-o fa-5x"></span>
+                  <h3>Follow Any Sport</h3>
+                  <p class="text-muted">From Football to Hockey, NFL to Basketball. We support it, and we're always adding new teams.</p>
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="feature-item">
+                  <span class="fa fa-fw fa-mobile fa-5x"></span>
+                  <h3>All Devices</h3>
+                  <p class="text-muted">View your calendar on the go with our Android App.</p>
+                </div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-md-6">
+                <div class="feature-item">
+                  <span class="fa fa-fw fa-calendar fa-5x"></span>
+                  <h3>Built For You</h3>
+                  <p class="text-muted">Every calendar is different, we only show you the events you care about.</p>
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="feature-item">
+                  <span class="fa fa-fw fa-usd fa-5x"></span>
+                  <h3>Free</h3>
+                  <p class="text-muted">Our app is completely free to use, we won't ever charge you to use it.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <?php include($_SERVER['DOCUMENT_ROOT'].'/assets/containers/footer.php'); ?>
+
+</body>
+</html>
